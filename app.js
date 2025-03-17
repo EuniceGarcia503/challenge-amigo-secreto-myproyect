01b2e247dfadaf14b659711f0b1a1b60bf8c1235
Eunice García <legd.cl26@gmail.com>
2025-03-17 17:26:01 -0300

    Creando función agregar nombre : agregarAmigo()

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 //TAREA 1********** CREAR ARRAY PARA ALMACENAR LOS NOMBRES**********//
 
 let listaAmigos = [];
+
+//TAREA 2********** CREAR FUNCIÓN AGREGAR AMIGOS************//
+
+ function agregarAmigo() {
+  
+    return;
+
+ }
